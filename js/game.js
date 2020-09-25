5808cb6534aa23345fc3448b19f0c09751a374ac
Quinten <me@quintenjames.com>
2020-09-24 22:19:56 -0600

    Highscores using localstorage. Draw high scores.

--- js/game.js
+++ js/game.js
@@ -52,6 +52,7 @@ function tick() {
     }
   }
   if (player.dead) {
+    addScore(score, false);
     gameState = "dead";
   }
   spawnCounter--;
@@ -70,7 +71,8 @@ function showTitle() {
 
   drawText("TWISTED VISION", 40, true, canvas.height / 2 - 110, "white");
   drawText("ROGUE LIKE", 70, true, canvas.height / 2 - 50, "white");
-  drawText("Press Any Key To Start", 20, true, canvas.height / 2 - 5, "white");
+
+  drawScores();
 }
 
 function startGame() {
@@ -104,4 +106,64 @@ function drawText(text, size, centered, textY, color) {
   }
 
   ctx.fillText(text, textX, textY);
+}
+
+function getScores() {
+  if (localStorage["scores"]) {
+    return JSON.parse(localStorage["scores"]);
+  } else {
+    return [];
+  }
+}
+
+function addScore(score, won) {
+  let scores = getScores();
+  let scoreObject = { score: score, run: 1, totalScore: score, active: won };
+  let lastScore = scores.pop();
+
+  if (lastScore) {
+    if (lastScore.active) {
+      scoreObject.run = lastScore.run + 1;
+      scoreObject.totalScore += lastScore.totalScore;
+    } else {
+      scores.push(lastScore);
+    }
+  }
+  scores.push(scoreObject);
+
+  localStorage["scores"] = JSON.stringify(scores);
+}
+
+function drawScores() {
+  let scores = getScores();
+  if (scores.length) {
+    drawText(
+      rightPad(["RUN", "SCORE", "TOTAL"]),
+      18,
+      true,
+      canvas.height / 2,
+      "white"
+    );
+
+    let newestScore = scores.pop();
+    scores.sort(function (a, b) {
+      return b.totalScore - a.totalScore;
+    });
+    scores.unshift(newestScore);
+
+    for (let i = 0; i < Math.min(10, scores.length); i++) {
+      let scoreText = rightPad([
+        scores[i].run,
+        scores[i].score,
+        scores[i].totalScore,
+      ]);
+      drawText(
+        scoreText,
+        18,
+        true,
+        canvas.height / 2 + 24 + i * 24,
+        i == 0 ? "aqua" : "violet"
+      );
+    }
+  }
 }
